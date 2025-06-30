79b9f1cc01a42dc0e2eb1cdba9e61db0422bc35c
kusqaum <kusqaum@gmail.com>
2025-06-30 12:55:37 +0100

    upload updated files

--- 1_cleanData.R
+++ 1_cleanData.R
@@ -1,5 +1,3 @@
-library(mice)
-
 
 setwd("~/Documents/GitHub/PDAC")
 
@@ -24,17 +22,34 @@ espac3 <- espac3[complete.cases(espac3),]
 
 espac3$PostOpCA199 <- log(espac3$PostOpCA199+1)
 espac3$MaxTumSiz <- sqrt(espac3$MaxTumSiz)
-espac3$LymphN <- as.factor(espac3$LymphN)
-espac3$Diff_Status <- as.factor(espac3$Diff_Status)
+
 espac3$PostOpCA199 <- as.numeric(espac3$PostOpCA199)
 espac3$OS_cen <- as.numeric(espac3$OS_cen)
 
 espac3 <- espac3[-which(espac3$Diff_Status == "Undifferentialted"),]
 
 
-espac3$Diff_Status <- droplevels(espac3$Diff_Status)
+#espac3$Diff_Status <- droplevels(espac3$Diff_Status)
 
 espac3[1:3,]
+
+# convert 
+espac3$LymphN <- as.factor(gsub("Neg", "0", espac3$LymphN))
+espac3$LymphN <- as.factor(gsub("Pos", "1", espac3$LymphN))
+espac3$ResecM <- as.factor(gsub("Neg", "0", espac3$ResecM))
+espac3$ResecM <- as.factor(gsub("Pos", "1", espac3$ResecM))
+espac3$LocalInv <- as.factor(gsub("No", "1", espac3$LocalInv))
+
+espac3$Diabetic[espac3$Diabetic == "No"] <- 0
+espac3$Diabetic[espac3$Diabetic == "Non Insulin Dependent"] <- 1
+espac3$Diabetic[espac3$Diabetic == "Insulin Dependent"] <- 2
+espac3$Diabetic <- as.factor(espac3$Diabetic)
+
+espac3$Diff_Status[espac3$Diff_Status=="Poor"] <-0
+espac3$Diff_Status[espac3$Diff_Status=="Moderate"] <- 1
+espac3$Diff_Status[espac3$Diff_Status=="Well"] <- 2
+espac3$Diff_Status <- as.factor(espac3$Diff_Status)
+
 
 saveRDS(espac3,  "Data/espac3clean.rds")
 
