5a277f0b9e4bf0354f226c87c8090f8541422627
kusqaum <kusqaum@gmail.com>
2025-07-24 15:42:29 +0100

    do sub-group analysis

--- gem_vs_gemcap.R
+++ gem_vs_gemcap.R
@@ -27,3 +27,34 @@ plot(pscfit) +
 dev.off()
 summary(pscfit)
 post <- pscfit$posterior
+
+
+#### sub-group analysis ####
+espac4_R1 <- espac4_gemcap[which(espac4_gemcap$ResecM==1),]
+espac4_R0 <- espac4_gemcap[which(espac4_gemcap$ResecM==0),]
+espac4_L1 <- espac4_gemcap[which(espac4_gemcap$LymphN==1),]
+espac4_L0 <- espac4_gemcap[which(espac4_gemcap$LymphN==0),]
+espac4_Diff0 <- espac4_gemcap[which(espac4_gemcap$Diff_Status==0),]
+espac4_Diff1 <- espac4_gemcap[which(espac4_gemcap$Diff_Status==1),]
+espac4_Diff2 <- espac4_gemcap[which(espac4_gemcap$Diff_Status==2),]
+espac4_CA19low <- espac4_gemcap[which(espac4_gemcap$PostOpCA199 < 20),]
+#espac4_CA19high <- espac4_gemcap[which(espac4_gemcap$PostOpCA199 >),]
+
+pscR1 <- pscfit(cfm, espac4_R1)
+pscR0 <- pscfit(cfm, espac4_R0)
+pscL1 <- pscfit(cfm, espac4_L1)
+pscL0 <- pscfit(cfm, espac4_L0)
+pscDiff0 <- pscfit(cfm, espac4_Diff0)
+pscDiff1 <- pscfit(cfm, espac4_Diff1)
+pscDiff2 <- pscfit(cfm, espac4_Diff2)
+# psc
+
+# r0 <- coefficients(pscR0)
+# temp <- as.data.frame(coefficients(pscR0))
+# b <- paste(round(r0[1,1],2), " (",round(r0[1,2],2), ",", round(r0[1,3],2),")", sep = "")
+# dic <- paste(round(r0[2,1],2), " (",round(r0[2,2],2), ",", round(r0[2,3],2),")", sep = "")
+# med <- rbind(b, dic); rownames(med) <- c("beta", "DIC"); colnames(med)<- c("median (2.5%, 7.5%")
+# kable(med, "html")
+# summPSCfit(pscR1)
+# coef(pscR1
+#      )
