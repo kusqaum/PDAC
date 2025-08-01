a7c53633b57accafcb18a43e732084b7d459139d
kusqaum <kusqaum@gmail.com>
2025-08-01 09:58:33 +0100

    add standard errors

--- Code/summ_cox_html.R
+++ Code/summ_cox_html.R
@@ -1,8 +1,9 @@
 summ_cox_html <- function(cm){
   library(knitr)
   res <- summary(cm)$conf.int
+  resest <- summary(cm)$coefficients
   xhr <- paste(round(res[,1],2), " (", round(res[,3],2), "-", round(res[,4],2), ")", sep = "")
-  xest  <- round(xcmrg$coefficients, 2) #;names(xest) <- NULL
+  xest  <- paste(round(cm$coefficients, 2), " (", round(resest[,3],2), ")", sep = "") #;names(xest) <- NULL
   cm_kab <- cbind(xest,xhr)
   colnames(cm_kab) <- c("Est (se)", "HR (95% CI)")
   kable(cm_kab, "html")
