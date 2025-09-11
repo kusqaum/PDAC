03278cfec858b380bedfbd8854ed1903579de60e
kusqaum <kusqaum1@gmail.com>
2025-09-11 14:56:56 +0100

    update files

--- gem_vs_gemcap.R
+++ gem_vs_gemcap.R
@@ -1,29 +1,20 @@
 library(psc)
-library(Cairo)
+library(ragg)
 library(ggplot2)
 library(survival)
 # compare gemcap data against gem
 load("Data/espac4gemcap.R")
 load("Output/Models/cfm.Rds")
+source("Code/discrimKMplot.R")
 
 summary(cfm)
 
 pscfit <-pscfit(CFM = cfm,DC = espac4_gemcap)
 
-CairoPNG("Output/Images/e3gem_vs_e4gemcap.png",
+agg_png("Output/Images/e3gem_vs_e4gemcap.png",res = 110,
          width = 600, height = 600, bg = "transparent")
 plot(pscfit) + 
-  theme(panel.background = element_rect(fill='transparent'), 
-        plot.background = element_rect(fill='transparent', colour=NA),
-        legend.background = element_rect(fill='transparent'), 
-        legend.box.background = element_rect(fill='transparent'),
-        axis.title.x = element_text(face="italic", colour="white"),
-        axis.title.y = element_text(face = "italic", colour="white"),
-        legend.text = element_text(colour="white"),
-        axis.line.x = element_line(colour = "white"),
-        axis.line.y = element_line(colour = "white"),
-        axis.text.x = element_text(colour = "white"),
-        axis.text.y = element_text(colour = "white"))
+  theme_mecPortal()
 dev.off()
 summary(pscfit)
 post <- pscfit$posterior
